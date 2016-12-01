07d9b3ffadf525edaef942c8f46f1abd51c9b9bc
Martin Petzold <Martin.Petzold.Official@gmail.com>
2016-12-01 19:32:27 +0100

    Start methods for different enviroments andapplication.yml

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,6 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,10 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@Configuration
 @ComponentScan
 @EnableScheduling
 @EnableAutoConfiguration
+@ConfigurationProperties
+@EnableConfigurationProperties
 
 public class Application {
     @RequestMapping("/")
