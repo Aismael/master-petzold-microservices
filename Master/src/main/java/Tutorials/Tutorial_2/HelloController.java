234144a1167803c2b5897b9285bc176a184fb6d1
Martin Petzold <Martin.Petzold.Official@gmail.com>
2016-12-07 20:33:15 +0100

    new out speach

--- Master/src/main/java/Tutorials/Tutorial_2/HelloController.java
+++ Master/src/main/java/Tutorials/Tutorial_2/HelloController.java
@@ -1,8 +1,5 @@
 package Tutorials.Tutorial_2;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
@@ -10,7 +7,7 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot over Docker!";
     }
 
 }
--- Master/src/test/java/Tutorials/Tutorial_5/ApplicationTest.java
+++ Master/src/test/java/Tutorials/Tutorial_5/ApplicationTest.java
@@ -1,7 +1,15 @@
 package Tutorials.Tutorial_5;
 import Tutorials.Tutorial_1.Application;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ApplicationTest {
