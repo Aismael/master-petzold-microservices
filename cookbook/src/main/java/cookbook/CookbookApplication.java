c317d859105afff96f9a2e95f1660ec1746ad74e
Aismael D <aismaelinc@web.de>
2017-03-30 11:21:05 +0200

    Cookbook local Hello World

--- cookbook/src/main/java/cookbook/CookbookApplication.java
+++ cookbook/src/main/java/cookbook/CookbookApplication.java
@@ -2,9 +2,21 @@ package cookbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@ConfigurationProperties
+@EnableConfigurationProperties
 @SpringBootApplication
 public class CookbookApplication {
+
+	@RequestMapping("/")
+    public String hello(){
+	    return "Hello World";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CookbookApplication.class, args);
