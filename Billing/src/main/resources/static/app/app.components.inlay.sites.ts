31336989633569e3822f34a439323803dd0e747c
Martin Petzold <Martin.Petzold.Official@gmail.com>
2017-05-27 23:10:13 +0200

    gitignore fix step 3

--- Billing/src/main/resources/static/app/app.components.inlay.sites.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-empty',
-    template: `<contentx></contentx>
-    <router-outlet></router-outlet>`
-})
-export class emptyComponent { }
--- Order/src/main/resources/db/migration/data.sql
+++
@@ -1,2 +0,0 @@
-insert into todo (checked, description) values (0, 'Roll an immense boulder up a hill.');
-insert into todo (checked, description) values (1, 'Roll an immense boulder down a hill.');
